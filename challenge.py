32bc42c02d2c84d958ed639f461a207939e50e07
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-01-04 21:26:18 +0800

    Add checking conditions and return Desert objects for all operations

--- challenge.py
+++ challenge.py
@@ -1,17 +1,18 @@
 """Solution to the challenge."""
 
+MAX_FUEL = 3
+
 
 class Desert():
-    """Class to define a challenge state."""
+    """Class to define the desert in the challenge."""
 
-    def __init__(self, n_target=10):
+    def __init__(self, n_target):
         # desert's state
         self.target = n_target
         self.desert_fuel = {k: 0 for k in range(1, n_target + 1)}
 
         # truck's state
         self.truck_fuel = 0
-        self.truck_max_fuel = 3
         self.truck_pos = 0
 
     def __str__(self):
@@ -19,23 +20,41 @@ class Desert():
             self.desert_fuel, self.truck_pos, self.truck_fuel)
         return _str
 
+    def __eq__(self, other):
+        return self.target == other.target and self.truck_pos == other.truck_pos
+
     def move_right(self):
         """Move the truck right, by one unit."""
-        self.truck_pos += 1
-        self.truck_fuel -= 1
+        if self.truck_fuel > 0:
+            self.truck_pos += 1
+            self.truck_fuel -= 1
+        return self
 
     def move_left(self):
         """Move the truck left, by one unit."""
-        self.truck_pos -= 1
-        self.truck_fuel -= 1
+        if self.truck_fuel > 0 and self.truck_pos > 0:
+            self.truck_pos -= 1
+            self.truck_fuel -= 1
+        return self
 
-    def load_fuel(self, n_fuel):
-        """Load the truck with fuel, by n_fuel units, until max capacity."""
-        self.truck_fuel = min(self.truck_max_fuel, self.truck_fuel + n_fuel)
+    def load_fuel(self):
+        """Load the truck with the maximum fuel allowed."""
+        if self.truck_pos == 0:
+            self.truck_fuel = MAX_FUEL
+        else:
+            cur_pos_fuel = self.desert_fuel[self.truck_pos]
+            max_allowed = max(cur_pos_fuel, MAX_FUEL - self.truck_fuel)
+            self.truck_fuel += max_allowed
+            self.desert_fuel[self.truck_pos] -= max_allowed
+        return self
 
-    def drop_fuel(self, n_fuel):
-        """Drop fuel from the truck, by n_fuel units, until 0."""
-        self.truck_fuel = max(0, self.truck_fuel - n_fuel)
+    def drop_fuel(self):
+        """Drop the maximum amount of fuel from the truck."""
+        if self.truck_pos > 0 and self.truck_fuel > 0:
+            max_allowed = self.truck_fuel
+            self.truck_fuel = 0
+            self.desert_fuel[self.truck_pos] += max_allowed
+        return self
 
 
 if __name__ == '__main__':
