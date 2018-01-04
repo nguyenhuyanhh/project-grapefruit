52886e091a0647e2cf877ad675ee2e5e91e2f13b
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-01-04 22:08:45 +0800

    Class State() and testing

--- challenge.py
+++ challenge.py
@@ -3,7 +3,7 @@
 MAX_FUEL = 3
 
 
-class Desert():
+class Desert:
     """Class to define the desert in the challenge."""
 
     def __init__(self, n_target):
@@ -55,6 +55,35 @@ class Desert():
             self.truck_fuel = 0
             self.desert_fuel[self.truck_pos] += max_allowed
         return self
+
+
+class State:
+    """Class to hold a state for the search problem."""
+
+    def __init__(self, init_desert):
+        self.desert = init_desert
+
+    def __str__(self):
+        return str(self.desert)
+
+    def __eq__(self, other):
+        return self.desert == other.desert
+
+    def _move_right(self):
+        return State(self.desert.move_right())
+
+    def _move_left(self):
+        return State(self.desert.move_left())
+
+    def _load_fuel(self):
+        return State(self.desert.load_fuel())
+
+    def _drop_fuel(self):
+        return State(self.desert.drop_fuel())
+
+    def next_states(self):
+        """Return the next states for the search problem."""
+        return [self._move_right(), self._move_left(), self._load_fuel(), self._drop_fuel()]
 
 
 if __name__ == '__main__':
--- challenge_tests.py
+++ challenge_tests.py
@@ -2,11 +2,11 @@
 
 from unittest import TestCase, main
 
-from challenge import Desert
+from challenge import Desert, State
 
 
 class TestDesert(TestCase):
-    """Testing for class Desert."""
+    """Test cases for class Desert."""
 
     def setUp(self):
         self.desert = Desert(n_target=5)
@@ -54,6 +54,22 @@ class TestDesert(TestCase):
         self.assertEqual(result.truck_pos, 1)
         self.assertEqual(result.truck_fuel, 0)
         self.assertEqual(result.desert_fuel[1], 2)
+
+
+class TestState(TestCase):
+    """Test cases for class State."""
+
+    def setUp(self):
+        self.desert = Desert(5)
+        self.state = State(self.desert)
+
+    def test_next_states_initial(self):
+        """Test the next states after the initial state."""
+        right, left, load, drop = self.state.next_states()
+        self.assertEqual(right.desert, self.desert)
+        self.assertEqual(left.desert, self.desert)
+        self.assertEqual(load.desert.truck_fuel, 3)
+        self.assertEqual(drop.desert, self.desert)
 
 
 if __name__ == '__main__':
