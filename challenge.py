4db72df01e6e6bf86394b57446634461b71217e0
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-01-05 16:34:09 +0800

    Remove State class, redundant

--- challenge.py
+++ challenge.py
@@ -41,7 +41,9 @@ class Desert:
         if values[-2] > 0:
             values[-1] += 1
             values[-2] -= 1
-        return values
+        else:
+            return None
+        return Desert(values)
 
     def move_left(self):
         """Move the truck left, by one unit."""
@@ -49,14 +51,18 @@ class Desert:
         if values[-2] > 0 and values[-1] > 0:
             values[-1] -= 1
             values[-2] -= 1
-        return values
+        else:
+            return None
+        return Desert(values)
 
     def load_fuel_base(self):
         """Load the truck with the max fuel allowed at base."""
         values = self.values[:]
         if values[-1] == 0:
             values[-2] = MAX_FUEL
-        return values
+        else:
+            return None
+        return Desert(values)
 
     def load_fuel_1(self):
         """Load the truck with 1 unit of fuel at any other position."""
@@ -65,7 +71,9 @@ class Desert:
         if cur_pos >= 0 and values[-2] + 1 <= MAX_FUEL and values[cur_pos] >= 1:
             values[cur_pos] -= 1
             values[-2] += 1
-        return values
+        else:
+            return None
+        return Desert(values)
 
     def load_fuel_2(self):
         """Load the truck with 2 units of fuel at any other position."""
@@ -74,7 +82,9 @@ class Desert:
         if cur_pos >= 0 and values[-2] + 2 <= MAX_FUEL and values[cur_pos] >= 2:
             values[cur_pos] -= 2
             values[-2] += 2
-        return values
+        else:
+            return None
+        return Desert(values)
 
     def drop_fuel(self):
         """Drop the maximum amount of fuel from the truck."""
@@ -83,82 +93,39 @@ class Desert:
             cur_pos = values[-1] - 1
             max_allowed, values[-2] = values[-2], 0
             values[cur_pos] += max_allowed
-        return values
-
-
-class State:
-    """Class to hold a state for the search problem."""
-
-    def __init__(self, desert_values):
-        self.desert = Desert(desert_values)
-
-    def __eq__(self, other):
-        return self.desert == other.desert
-
-    def __str__(self):
-        return str(self.desert)
-
-    def __hash__(self):
-        return hash(str(self))
-
-    def pretty_print(self):
-        """Pretty-printed representation of the state."""
-        return self.desert.pretty_print()
-
-    def move_right(self):
-        """Move the truck right, by one unit."""
-        result = self.desert.move_right()
-        return State(result)
-
-    def move_left(self):
-        """Move the truck left, by one unit."""
-        result = self.desert.move_left()
-        return State(result)
-
-    def load_fuel_base(self):
-        """Load the truck with the maximum fuel allowed."""
-        result = self.desert.load_fuel_base()
-        return State(result)
-
-    def load_fuel_1(self):
-        """Load the truck with 1 unit of fuel at any other location."""
-        result = self.desert.load_fuel_1()
-        return State(result)
-
-    def load_fuel_2(self):
-        """Load the truck with 2 units of fuel at any other location."""
-        result = self.desert.load_fuel_2()
-        return State(result)
-
-    def drop_fuel(self):
-        """Drop the maximum amount of fuel from the truck."""
-        result = self.desert.drop_fuel()
-        return State(result)
+        else:
+            return None
+        return Desert(values)
 
     def next_states(self):
         """Return the next states for the search problem."""
-        result = OrderedDict([('F', self.load_fuel_base()),
-                              ('R', self.move_right()),
-                              ('E', self.drop_fuel()),
-                              ('1', self.load_fuel_1()),
-                              ('L', self.move_left())])
-        return result
+        result = OrderedDict()
+        if self.load_fuel_base():
+            result['F'] = self.load_fuel_base()
+        if self.move_right():
+            result['R'] = self.move_right()
+        if self.drop_fuel():
+            result['E'] = self.drop_fuel()
+        if self.load_fuel_1():
+            result['1'] = self.load_fuel_1()
+        if self.move_left():
+            result['L'] = self.move_left()
+        return result, True if result else False
 
 
 def bfs(target):
     """Use BFS to search for the goal state."""
 
     def _goal_state(state):
-        return str(state).split(',')[-1] == str(target)
+        return state.values[-1] == target
 
     # named tuple to keep track of search positions
     search_pos = namedtuple('search_pos', ['cur_state', 'prev_state', 'path'])
 
     # initialize the search problem
     init_value = [0 for i in range(target)] + [0, 0]
-    pos = search_pos(State(init_value), None, '')
-    queue = deque([pos])
-    explored = set()
+    pos = search_pos(Desert(init_value), None, '')
+    queue, explored = deque([pos]), set()
 
     # search
     while queue:
@@ -167,22 +134,23 @@ def bfs(target):
         if _goal_state(cur_state):
             return cur_pos
         explored.add(cur_state)
-        for key, next_state in cur_state.next_states().items():
-            cur_path = cur_pos.path
-            if next_state != cur_state and next_state not in explored and next_state not in [x.cur_state for x in queue]:
-                queue.append(search_pos(
-                    next_state, cur_state, cur_path + key))
+        next_states, next_test = cur_state.next_states()
+        if next_test:
+            for key, next_state in next_states.items():
+                cur_path = cur_pos.path
+                if next_state != cur_state and next_state not in explored and \
+                        next_state not in [x.cur_state for x in queue]:
+                    queue.append(search_pos(
+                        next_state, cur_state, cur_path + key))
 
 
 def interactive(init_state, sequence):
-    """Interactive mode."""
+    """Interactive mode, print out the state after applying a sequence of moves."""
     queue = deque(sequence)
     state = init_state
-
     while queue:
         move = queue.popleft()
         state = state.next_states()[move]
-
     return state
 
 
@@ -194,4 +162,4 @@ if __name__ == '__main__':
 
     elif len(sys.argv) == 3:
         init_values = [0 for i in range(int(sys.argv[1]))] + [0, 0]
-        print(interactive(State(init_values), sys.argv[2]).pretty_print())
+        print(interactive(Desert(init_values), sys.argv[2]).pretty_print())
